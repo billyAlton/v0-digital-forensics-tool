55ce7540f462808852725e16fba12b1787804b23
israelittiq <israel.bogninou@ittiq.net>
2025-10-29 13:35:51 +0100

    sf

--- lib/apiCaller.ts
+++ lib/apiCaller.ts
@@ -5,7 +5,7 @@ import axios, {
   InternalAxiosRequestConfig,
 } from "axios";
 // Base URL for the API
-export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL = "http://localhost:8002/api";
 
 // export const BASE_URL = 'https://codux.kababeats.com/api';
 
